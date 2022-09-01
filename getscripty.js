5d1f7e1baa52d0ba47e424a0e4447290e35baaf7
jleesys949 <jlee.post24@gmail.com>
2022-08-31 22:41:39 -0700

    Create computer move generator. Play game function

--- getscripty.js
+++ getscripty.js
@@ -1,7 +1,47 @@
 let getComputerChoice = () => {
     let arrChoices = ['rock','paper','scissors'];
     let rand = Math.floor(Math.random() * 3);
-    console.log(rand);
-    // let choice = arrChoices[Math.random() ]
-    // return choice;
+    // console.log(arrChoices[rand]);
+
+    return arrChoices[rand];
+}
+
+let playGame = (playerChoice,computerChoice) => {
+    playerChoice = playerChoice.toLowerCase();
+    console.log(playerChoice);
+
+    // DRAW BLOCK
+    if (playerChoice == computerChoice) {
+        return "It's a draw";
+    }
+    
+    // ROCK BLOCK
+    if (playerChoice == 'rock' && computerChoice == 'scissors') {
+        return `You win! You played Rock; Computer played Scissors.
+        Rock beats scissors!`;
+    }
+    if (playerChoice == 'rock' && computerChoice == 'paper') {
+        return `You lose! You played Rock; Computer played Paper.
+        Paper beats rock!`;
+    }
+
+    // PAPER BLOCK
+    if (playerChoice == 'paper' && computerChoice == 'rock') {
+        return `You win! You played Paper; Computer played Rock.
+        Paper beats rock!`;
+    }
+    if (playerChoice == 'paper' && computerChoice == 'scissors') {
+        return `You lose! You played Paper; Computer played Scissors.
+        Scissors beats paper!`;
+    }
+
+    // SCISSORS BLOCK
+    if (playerChoice == 'scissors' && computerChoice == 'rock') {
+        return `You lose! You played Scissors; Computer played Rock.
+        Rock beats scissors!`;
+    }
+    if (playerChoice == 'scissors' && computerChoice == 'paper') {
+        return `You win! You played Scissors; Computer played Paper.
+        Scissors beats paper!`;
+    }
 }
