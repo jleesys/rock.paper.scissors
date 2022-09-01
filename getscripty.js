40fe2f660bad770fe7281405b288ea646a9f5cbe
jleesys949 <jlee.post24@gmail.com>
2022-08-31 23:24:40 -0700

    Refactor return statements, play game function

--- getscripty.js
+++ getscripty.js
@@ -13,43 +13,53 @@ let playRound = (playerChoice,computerChoice) => {
     // DRAW BLOCK
     if (playerChoice == computerChoice) {
         let condition = 'draw';
-        return `It's a draw! You played ${playerChoice}. The computer played ${computerChoice}.`
+        console.log( `It's a draw! You played ${playerChoice}. The computer played ${computerChoice}.`);
+        return condition;
     }
     
     // ROCK BLOCK
     if (playerChoice == 'rock' && computerChoice == 'scissors') {
         let condition = 'win';
-        return `You ${condition}! You played ${playerChoice}. The computer played ${computerChoice}.`
+        console.log( `You ${condition}! You played ${playerChoice}. The computer played ${computerChoice}.`);
+        return condition;
     }
     if (playerChoice == 'rock' && computerChoice == 'paper') {
         let condition = 'lose';
-        return `You ${condition}! You played ${playerChoice}. The computer played ${computerChoice}.`
+        console.log( `You ${condition}! You played ${playerChoice}. The computer played ${computerChoice}.`);
+        return condition;
     }
 
     // PAPER BLOCK
     if (playerChoice == 'paper' && computerChoice == 'rock') {
         let condition = 'win';
-        return `You ${condition}! You played ${playerChoice}. The computer played ${computerChoice}.`
+        console.log( `You ${condition}! You played ${playerChoice}. The computer played ${computerChoice}.`);
+        return condition;
     }
     if (playerChoice == 'paper' && computerChoice == 'scissors') {
         let condition = 'lose';
-        return `You ${condition}! You played ${playerChoice}. The computer played ${computerChoice}.`
+        console.log( `You ${condition}! You played ${playerChoice}. The computer played ${computerChoice}.`);
+        return condition;
     }
 
     // SCISSORS BLOCK
     if (playerChoice == 'scissors' && computerChoice == 'rock') {
         let condition = 'lose';
-        return `You ${condition}! You played ${playerChoice}. The computer played ${computerChoice}.`
+        console.log( `You ${condition}! You played ${playerChoice}. The computer played ${computerChoice}.`);
+        return condition;
     }
     if (playerChoice == 'scissors' && computerChoice == 'paper') {
         let condition = 'win';
-        return `You ${condition}! You played ${playerChoice}. The computer played ${computerChoice}.`
+        console.log( `You ${condition}! You played ${playerChoice}. The computer played ${computerChoice}.`);
+        return condition;
     }
 }
 
 let playGame = () => {
     let score = 0;
     for (i = 0 ; i < 5 ; i++) {
-        console.log(playRound('rock',getComputerChoice()));
+        if (playRound('rock',getComputerChoice()) == 'win') {
+            score++;
+        } 
     }
+    console.log("Your score is " + score);
 }
