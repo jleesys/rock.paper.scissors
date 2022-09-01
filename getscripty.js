2019d02e2d1d38290bc0997c2432492a661b0e0c
jleesys949 <jlee.post24@gmail.com>
2022-08-31 22:21:59 -0700

    Create random num generator

---
+++ getscripty.js
@@ -0,0 +1,7 @@
+let getComputerChoice = () => {
+    let arrChoices = ['rock','paper','scissors'];
+    let rand = Math.floor(Math.random() * 3);
+    console.log(rand);
+    // let choice = arrChoices[Math.random() ]
+    // return choice;
+}
