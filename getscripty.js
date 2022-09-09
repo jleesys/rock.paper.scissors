0de39ba6d447b25b5e04e0a756cf6eefd6569935
jleesys949 <jlee.post24@gmail.com>
2022-09-09 16:33:29 -0700

    Fix logging

--- getscripty.js
+++ getscripty.js
@@ -1,4 +1,7 @@
 // Script with UI for click interactivity
+var score = 0;
+var gameNum = 1;
+
 const body = document.querySelector('body');
 const rockSelect = document.querySelector('#rock');
 const paperSelect = document.querySelector('#paper');
@@ -11,19 +14,19 @@ body.appendChild(scoreBoard);
 rockSelect.addEventListener('click', () => {
     const playerChoice = 'rock';
     const computerChoice = getComputerChoice();
-    playRound(playerChoice,computerChoice);
+    playRound(playerChoice, computerChoice);
     // scoreBoard.textContent = `You ` + playRound(playerChoice,computerChoice) + `!`;
 })
 paperSelect.addEventListener('click', () => {
     const playerChoice = 'paper';
     const computerChoice = getComputerChoice();
-    playRound(playerChoice,computerChoice);
+    playRound(playerChoice, computerChoice);
     // scoreBoard.textContent = `You ` + playRound(playerChoice,computerChoice) + `!`;
 })
 scissorsSelect.addEventListener('click', () => {
     const playerChoice = 'scissors';
     const computerChoice = getComputerChoice();
-    playRound(playerChoice,computerChoice);
+    playRound(playerChoice, computerChoice);
     // scoreBoard.textContent = `You ` + playRound(playerChoice,computerChoice) + `!`;
 })
 
@@ -36,6 +39,7 @@ for (const button in buttons) {
 }
 */
 
+/*
 // function asks the player for their move via input window
 function getPlayerChoice() {
     while (true) {
@@ -50,6 +54,7 @@ function getPlayerChoice() {
     }
     return playerMove;
 }
+*/
 
 // Generates a random integer between 0 and 2, and then returns
 // a move based on that number.
@@ -102,11 +107,9 @@ let playRound = (playerChoice, computerChoice) => {
 let playGame = () => {
     var score = 0;
     // alert("We're going to play a game! Best of 5 rounds! We're keeping score!");
-    // for (let i = 0; i < 5; i++) {
-        let outcome = playRound(getPlayerChoice(), getComputerChoice());
-        outcome == 'win' ? score++ : score;
+    for (let i = 0; i < 5; i++) {
         console.log(`Your current score is ${score}.`)
-    // }
+    }
     // console.log(`The final score is ${score}!`);
 }
 
